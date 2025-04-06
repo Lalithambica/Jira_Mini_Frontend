b4b6d36d5be18281d3a8fd333158ec92f936bf93
Lalithambica <lalithachepuri18@gmail.com>
2025-04-06 17:25:02 +0530

    Initial commit

---
+++ src/components/project/Details.tsx
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div className="mx-10">
+      <h1 className='mb-4 mt-7 text-xl font-semibold text-light-c-3'>Jira Board</h1>
+    </div>
+  );
+};
+
+export default Details;
---
+++ src/components/project/Project.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useIssuesQuery } from '../../api/endpoints/issues.endpoint';
+import { useListsQuery } from '../../api/endpoints/lists.endpoint';
+import type { APIERROR,Issue } from '../../api/apiTypes';
+import Board from './Board';
+import Filter from './Filter';
+import SS from '../util/SpinningCircle';
+import { useAppSelector } from '../../store/hooks';
+
+
+const Project = () => {
+  const projectId = Number(useParams().projectId);
+  const issueQuery = useAppSelector((state) => state.query.issue);
+  const { data: lists, error: listError } = useListsQuery(projectId);
+  const [isDragDisabled, setIsDragDisabled] = useState(false);
+  const [issueFilter,setIssueFilter] = useState("")
+
+  const { data: issues, error: issueError } = useIssuesQuery(
+    { projectId, ...issueQuery },
+    { refetchOnMountOrArgChange: true }
+  );
+
+  if (listError && issueError) {
+    if ((listError as APIERROR).status === 401 || (issueError as APIERROR).status === 401)
+      return <Navigate to='/login' />;
+    return (
+      <div className='grid h-full grow place-items-center text-xl'>
+        You are not part of this project ☝
+      </div>
+    );
+  }
+
+  return (
+    <div className='mt-6 flex grow flex-col px-8 sm:px-10'>
+      <h1 className='mb-4 text-xl font-semibold text-c-text'>Jira Board</h1>
+      <Filter isEmpty={lists?.length === 0} {...{ projectId, setIsDragDisabled }} issueFilter={issueFilter} setIssueFilter={setIssueFilter} />
+
+      {lists ? (
+        // Map over the issues and process them before passing to the Board
+        <Board
+          {...{
+            lists,
+            issueFilter,
+            issues : issues,
+            isDragDisabled,
+          }}
+        />
+      ) : (
+        <div className="grid h-[40vh] w-full place-items-center">
+          <SS />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Project;
